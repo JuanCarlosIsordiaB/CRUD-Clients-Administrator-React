3949dceca285b874aa8a0f39fcfc586b7c6d69f4
JuanCarlosIsordiaB <juancarlosisordiab@gmail.com>
2024-01-10 09:40:19 -0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import {} from 'react-router-dom';
+
+const App = () => {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route></Route>
+      </Routes>
+    </BrowserRouter>
+  )
+}
+export default App;
